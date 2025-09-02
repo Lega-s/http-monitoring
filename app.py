f8e123ae4251589dff12f84b76667c2426a0e44e
Levy schaerer <163449974+Lega-s@users.noreply.github.com>
2025-09-02 13:45:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,26 +3,8 @@ import plotly.express as px
 import pandas as pd
 from datetime import datetime, timedelta
 import textwrap
-import requests
-import time
 import csv
 import os
-
-# Requests components
-
-csv_file = 'data.csv'
-
-proxies = {
-    "http": "http://userproxy.pnet.ch:3128",
-    "https": "http://userproxy.pnet.ch:3128",
-}
-
-if not os.path.isfile(csv_file):
-    with open(csv_file, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['timestamp', 'latency_ms', 'error'])
-
-# Dash components
 
 dateparse = lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
 df = pd.read_csv('data.csv', parse_dates=['timestamp'], date_parser=dateparse)
@@ -104,49 +86,7 @@ def update_graph(selected_range, n_intervals):
         avg_text = f"Average Latency: {avg_latency:.2f} ms"
 
     return fig, avg_text
-
-@callback(
-    Output('interval-store', 'data'),
-    Input('interval-component', 'n_intervals')
-)
-def measure_load_time():
-    log = {};
-    start_time = time.time()
-
-    try:
-        response = requests.get("http://www.google.com", proxies=proxies, timeout=10)
-        latency = int((time.time() - start_time) * 1000)
-        log = {
-            "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            "latency_ms": latency,
-            "error": None
-        }
-
-        requests.post("http://10.226.0.166:5000/log", json=log)
-    
-    except requests.exceptions.RequestException as e:
-        log = {
-            "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            "latency_ms": 0,
-            "error": str(e)
-        }
-
-        requests.post("http://10.226.0.166:5000/log", json=log)
-
-    print(log)
-    if log["latency_ms"] > 500 or log["error"] is not None:
-        return 5000
-    else:
-        return 120000
-    
-@callback(
-    Output('interval-component', 'interval'),
-    Input('interval-store', 'data')
-)
-def update_interval(new_interval):
-    return new_interval
         
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8050, debug=True)
- 
