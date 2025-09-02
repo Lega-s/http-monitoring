5c8318298a9073b29d088c148b9ae6a6263fbfb3
Levy schaerer <163449974+Lega-s@users.noreply.github.com>
2025-09-02 13:46:01 +0200

    Add files via upload

---
+++ request.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+import requests
+import time
+
+sleep_time = 120
+
+proxies = {
+    "http": "http://userproxy.pnet.ch:3128",
+    "https": "http://userproxy.pnet.ch:3128",
+}
+
+def measure_load_time():
+    log = {};
+    start_time = time.time()
+
+    try:
+        response = requests.get("http://www.google.com", proxies=proxies, timeout=10)
+        latency = int((time.time() - start_time) * 1000)
+        log = {
+            "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            "latency_ms": latency,
+            "error": None
+        }
+
+        requests.post("http://10.226.0.166:5000/log", json=log)
+    
+    except requests.exceptions.RequestException as e:
+        log = {
+            "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            "latency_ms": 0,
+            "error": str(e)
+        }
+
+        requests.post("http://10.226.0.166:5000/log", json=log)
+
+    if log["latency_ms"] > 500 or log["error"] is not None:
+        sleep_time = 5
+    else:
+        sleep_time = 120
+    
+while (True):
+    measure_load_time()
+    time.sleep(sleep_time)
